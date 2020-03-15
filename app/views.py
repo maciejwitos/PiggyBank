109077091ba545746c27bda2f268ee1d5201e7a3
maciejwitos <mac.witos@gmail.com>
2020-03-15 18:20:44 +0100

    Add search transaction to dashboard

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from app.user.user_config import *
+from django.db.models import Q
 
 
 class Dashboard(LoginRequiredMixin, View):
@@ -8,6 +9,25 @@ class Dashboard(LoginRequiredMixin, View):
         categories = Category.objects.filter(user=request.user).order_by('-spending')
         currencies = Currency.objects.all()
         transactions = Transaction.objects.filter(user=request.user).order_by('-date')
+        accounts = Account.objects.filter(user=request.user).order_by('-balance')
+        my_wealth = 0
+        for account in accounts:
+            balance = account.balance
+            balance_in_pln = balance * account.currency.in_pln
+            my_wealth += balance_in_pln
+        return render(request, 'dashboard.html', {'categories': categories,
+                                                  'currencies': currencies,
+                                                  'transactions': transactions,
+                                                  'accounts': accounts,
+                                                  'my_wealth': float(my_wealth)})
+
+    def post(self, request):
+        data = request.POST.get('search_transaction')
+        categories = Category.objects.filter(user=request.user).order_by('-spending')
+        currencies = Currency.objects.all()
+        transactions = Transaction.objects.filter(user=request.user).filter(
+            Q(comment__icontains=data) | Q(date__icontains=data)).order_by('-date')
+
         accounts = Account.objects.filter(user=request.user).order_by('-balance')
         my_wealth = 0
         for account in accounts:
