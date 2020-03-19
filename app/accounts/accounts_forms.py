d7a95d3a52f478a9f0231d7d3f934e1fddd24183
maciejwitos <mac.witos@gmail.com>
2020-03-19 14:09:28 +0100

    Change category, account forms

--- app/accounts/accounts_forms.py
+++ app/accounts/accounts_forms.py
@@ -3,8 +3,8 @@ from app.forms import *
 
 
 class AddAccountForm(forms.Form):
-    name = forms.CharField(max_length=16)
-    bank = forms.CharField(max_length=16)
-    balance = forms.FloatField()
+    name = forms.CharField(max_length=24, label='Nazwa', widget=(forms.TextInput({'class':'form-control'})))
+    bank = forms.CharField(max_length=12, label='Bank', widget=(forms.TextInput({'class':'form-control'})))
+    balance = forms.FloatField(label='Saldo', widget=(forms.NumberInput({'class':'form-control'})))
     user = forms.HiddenInput()
-    currency = forms.ModelChoiceField(queryset=Currency.objects.all())
+    currency = forms.ModelChoiceField(queryset=Currency.objects.all(), label='Waluta', widget=(forms.Select({'class':'form-control'})))
--- app/category/category_forms.py
+++ app/category/category_forms.py
@@ -2,6 +2,6 @@ from app.forms import *
 
 
 class AddCategoryForm(forms.Form):
-    name = forms.CharField(max_length=16)
+    name = forms.CharField(max_length=24, label='Nazwa', widget=forms.TextInput({'class':'form-control'}))
     spending = forms.HiddenInput()
     user = forms.HiddenInput()
